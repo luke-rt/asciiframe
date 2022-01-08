0ca195e2d0427970177e4bd4dad25c1fc9fb07ca
ltgr <luke.rtong@gmail.com>
2022-01-08 14:15:22 -0500

    Made ci scripts more efficient

--- ci/deps-macos.sh
+++ ci/deps-macos.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 brew install opencv
+export DYLD_FALLBACK_LIBRARY_PATH="$(xcode-select --print-path)/Toolchains/XcodeDefault.xctoolchain/usr/lib/"
--- ci/deps.sh
+++ ci/deps.sh
@@ -6,7 +6,12 @@ ci_dir="$(dirname "$0")"
 
 case "$OSTYPE" in
     darwin*)
-        "$ci_dir/deps-macos.sh" ;;
+        "$ci_dir/deps-macos.sh"
+        echo "=== Installed brew packages:"
+	    brew list --versions ;;
     linux*)
         "$ci_dir/deps-linux.sh" ;;
 esac
+
+echo "=== Environment variable dump:"
+export
