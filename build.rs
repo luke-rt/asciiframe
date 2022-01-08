c4ee834e320a82970c05c925c367323855e1e9e5
ltgr <luke.rtong@gmail.com>
2022-01-08 15:09:41 -0500

    refactoring

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
+#[cfg(target_os = "linux")]
+fn main() {}
+
 #[cfg(target_os = "macos")]
 fn main() {
     ::std::env::set_var("DYLD_FALLBACK_LIBRARY_PATH", "$(xcode-select --print-path)/Toolchains/XcodeDefault.xctoolchain/usr/lib/");
 }
+
+#[cfg(target_os = "windows")]
+fn main() {}
