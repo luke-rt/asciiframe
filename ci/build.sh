0cc7587212d46d8641a4327e468231bb7cc19ad7
ltgr <luke.rtong@gmail.com>
2022-01-08 23:49:55 -0500

    fixed build script

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+set -vex
+
 case "$OSTYPE" in
     darwin*)
-        export DYLD_FALLBACK_LIBRARY_PATH = "$(xcode-select --print-path)/Toolchains/XcodeDefault.xctoolchain/usr/lib/"
+        export DYLD_FALLBACK_LIBRARY_PATH="$(xcode-select --print-path)/Toolchains/XcodeDefault.xctoolchain/usr/lib/"
         cargo build --verbose --release --locked --target=x86_64-apple-darwin ;;
     linux*)
         cargo build --verbose --release --locked --target=x86_64-unknown-linux-gnu ;;
