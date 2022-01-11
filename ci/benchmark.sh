74859c3631345f23213c5e51d583974ed47b025b
ltgr <luke.rtong@gmail.com>
2022-01-10 20:51:11 -0500

    Fixed syntax error

--- ci/benchmark.sh
+++ ci/benchmark.sh
@@ -4,7 +4,7 @@ set -vex
 
 case "$OSTYPE" in
     darwin*)
-        brew install hyperfine
+        brew install hyperfine ;;
     linux*)
         wget https://github.com/sharkdp/hyperfine/releases/download/v1.12.0/hyperfine_1.12.0_amd64.deb
         sudo dpkg -i hyperfine_1.12.0_amd64.deb ;;
