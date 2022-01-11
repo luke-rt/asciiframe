c0a53a51d919a18ea3556ed82e59a61c2ce4e85e
ltgr <luke.rtong@gmail.com>
2022-01-11 15:53:39 -0500

    Better terminal clearing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![warn(warnings, rust_2018_idioms)]
 #![warn(clippy::all, clippy::pedantic)]
 #![allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
+use std::io::{self, Write};
 
 use ansi_term::Colour::{Green, Red};
 use clap::Parser;
@@ -23,7 +24,15 @@ pub fn main() {
 	if let Err(e) =
 		renderer::render(&opts.file, opts.output.as_deref(), strategy)
 	{
-		eprintln!("{} {}", Red.paint("error:"), e);
-		eprintln!("\nFor more information try {}", Green.paint("--help"));
+		let stderr = io::stderr();
+		let mut handle = stderr.lock();
+		write!(
+			handle,
+			"{} {}\n\nFor more information try {}",
+			Red.paint("error:"),
+			e,
+			Green.paint("--help")
+		)
+		.unwrap();
 	}
 }
