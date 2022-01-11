060518642babd729fcc06e950aea9d37f703fbcb
ltgr <luke.rtong@gmail.com>
2022-01-10 20:44:20 -0500

    Fixed file references

--- ci/benchmark.sh
+++ ci/benchmark.sh
@@ -4,11 +4,12 @@ set -vex
 
 case "$OSTYPE" in
     darwin*)
-        brew install hyperfine
+        echo "hello" ;;
+        # brew install hyperfine
     linux*)
         wget https://github.com/sharkdp/hyperfine/releases/download/v1.12.0/hyperfine_1.12.0_amd64.deb
-        sudo dpkg -i hyperfine_1.12.0_amd64.deb
+        sudo dpkg -i hyperfine_1.12.0_amd64.deb ;;
 esac
 
 echo "=== BENCHMARK RESULTS:"
-hyperfine --warmup 3 --prepare "rm -r benchmarks/test.sh" "asc -f benchmarks/test.mov -o benchmarks/test.sh"
+hyperfine --warmup 3 --prepare "rm -r ci/benchmarks/test.sh || true" "asc -f ci/benchmarks/test.mov -o ci/benchmarks/test.sh"
---
+++ ci/benchmarks/test.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a3ca08aad955af56f92d62688d27bc9da2bc9cb3beaeaaf12bf2ecded55a8dc
+size 56
