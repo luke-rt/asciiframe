cc363cbab24c452e7680687ae6a7570c59308260
ltgr <luke.rtong@gmail.com>
2022-01-10 20:44:55 -0500

    Removed comment

--- ci/benchmark.sh
+++ ci/benchmark.sh
@@ -4,8 +4,7 @@ set -vex
 
 case "$OSTYPE" in
     darwin*)
-        echo "hello" ;;
-        # brew install hyperfine
+        brew install hyperfine
     linux*)
         wget https://github.com/sharkdp/hyperfine/releases/download/v1.12.0/hyperfine_1.12.0_amd64.deb
         sudo dpkg -i hyperfine_1.12.0_amd64.deb ;;
